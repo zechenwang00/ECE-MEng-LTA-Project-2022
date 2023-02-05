de4ff06f5c62fb47b8fda9ee307f0711aafbd548
Zechen Wang <zechenwang@hotmail.com>
2023-02-05 17:30:49 -0500

    added pose estimation

--- at_test.py
+++ at_test.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import Apriltag
 from Apriltag import Detector
+import math
 
 
 cam = cv2.VideoCapture(0)
@@ -18,6 +19,10 @@ if not cam.isOpened():
     print("cannot access camera 0")
     exit()
 
+# width = cam.get(3)
+# height = cam.get(4)
+# print(width, height)
+
 print("start detecting")
 while True:
     # read from camera
@@ -25,19 +30,47 @@ while True:
     if not ret:
         break
 
+    # camera info, resolution 640*480
+    focal = 528.7
+    cx = 320
+    cy = 240
+
+
     # detect on greyscale
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray = cv2.resize(gray, (640, 360))
     # cv2.imshow('frame', gray)
 
-    detections = at_detector.detect(gray)
+    detections = at_detector.detect(gray, estimate_tag_pose=True,
+                                          camera_params=[focal, focal, cx, cy],
+                                          tag_size=0.175)
     # format output
     if len(detections) >= 1:
         for idx in range(len(detections)):
-            print("Detected tag id[" + str(detections[idx].tag_id), end='] @ ')
-            print('x = '  + str(round(detections[idx].center[0])) +
-                  ' y = ' + str(round(detections[idx].center[1])) )
-            print(dir(detections[idx]), '\n\n')
+            # process tag info
+            tag_id = detections[idx].tag_id
+            center_x_pixel = detections[idx].center[0]
+            center_y_pixel = detections[idx].center[1]
+
+            # process translation/rotation
+            translation_vector = detections[idx].pose_t
+            # rotation_vector  = detections[idx].pose_R
+            # err_vector       = detections[idx].pose_err
+            x_translation = translation_vector[0][0]
+            y_translation = translation_vector[1][0]
+            z_translation = translation_vector[2][0]
+
+            # print output
+            print("Detected tag id[", tag_id, ']')
+            # print('x center pixel: ', center_x_pixel,' y center pixel: ', center_y_pixel)
+            print("x:{:.2f} ".format(x_translation),
+                  "y:{:.2f} ".format(y_translation),
+                  "z:{:.2f}" .format(z_translation))
+
+            # print("pose_R:", detections[idx].pose_R)
+            # print("pose_T:", detections[idx].pose_t)
+            # print("pose_err:", detections[idx].pose_err)
+
+            print("\n")
 
     # break
     if cv2.waitKey(1) == ord('q'):
@@ -47,4 +80,3 @@ while True:
 # When everything done, release the capture
 cam.release()
 cv2.destroyAllWindows()
-
